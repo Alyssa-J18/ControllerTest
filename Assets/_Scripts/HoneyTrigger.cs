31ff64b30f92b2252d39084b4c5b331357b6c984
Alyssa-J18 <ajacoby1118@gmail.com>
2026-02-25 15:58:51 -0500

    ooh i love my wiiiife i love my wife AH (fixed level difficulty and froze z rotation)

--- Assets/_Scripts/HoneyTrigger.cs
+++ Assets/_Scripts/HoneyTrigger.cs
@@ -2,13 +2,13 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
-public class MushroomTrigger : MonoBehaviour
+public class HoneyTrigger : MonoBehaviour
 {
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-            Debug.Log("Player has triggered the mushroom!");
+            Debug.Log("Player has triggered the honey pot!");
             // Add additional logic here, e.g., increase score, play sound, etc.
             Destroy(gameObject);
         }
