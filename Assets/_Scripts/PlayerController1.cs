66ed24adfe4b7ad56b58e2395daeae0163df5d08
Alyssa-J18 <ajacoby1118@gmail.com>
2026-03-01 22:01:39 -0500

    fixed movement, expanded level, added enemy pathing

--- Assets/_Scripts/PlayerController1.cs
+++ Assets/_Scripts/PlayerController1.cs
@@ -3,18 +3,27 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 0.1f;
-    public float jumpHeight = 0.5f;
+    public Rigidbody2D rb;
+    public float jumpPower = 10f;
+    
+    public Transform groundCheckPos;
+    public Vector2 groundCheckSize = new Vector2(0.5f, 0.05f);
+    public LayerMask groundLayer;
+    public int maxJumps = 2;
+    int jumpsRemaining;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        jumpsRemaining = maxJumps;
     }
 
     // Update is called once per frame
     void Update()
     {
         MovePlayer();
+        GroundCheck();
     }
 
     void MovePlayer()
@@ -41,19 +50,28 @@ public class PlayerController : MonoBehaviour
             // Update the player's position
             gameObject.transform.position = newPos;
         }
-
-        if(Input.GetKey(KeyCode.Space))
+    if(jumpsRemaining > 0)
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
         {
-            // Get the current position of the player
-            Vector2 curPos = gameObject.transform.position;
-
-            // Get new position by subtracting from the x coordinate
-            Vector2 newPos = new Vector2(curPos.x, curPos.y + jumpHeight);
-
-            // Update the player's position
-            gameObject.transform.position = newPos;
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpPower);
+            jumpsRemaining--;
         }
-
-        
     }
+    }
+
+    private void GroundCheck()
+    {
+        if (Physics2D.OverlapBox(groundCheckPos.position, groundCheckSize, 0, groundLayer))
+        {
+            jumpsRemaining = maxJumps;
+        }
+    }
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.white;
+        Gizmos.DrawWireCube(groundCheckPos.position, groundCheckSize);
+    }
+        
+    
 }
