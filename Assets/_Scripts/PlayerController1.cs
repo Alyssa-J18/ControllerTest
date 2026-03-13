ac12039f39eee41d4b9d3037e77c2a02843ae058
Alyssa-J18 <ajacoby1118@gmail.com>
2026-03-13 18:04:49 -0400

    fixed slope and added the start of the light mechanic

--- Assets/_Scripts/PlayerController1.cs
+++ Assets/_Scripts/PlayerController1.cs
@@ -7,7 +7,7 @@ public class PlayerController : MonoBehaviour
     public float jumpPower = 10f;
     
     public Transform groundCheckPos;
-    public Vector2 groundCheckSize = new Vector2(0.5f, 0.05f);
+    public Vector2 groundCheckSize = new Vector2(0.6f, 0.2f);
     public LayerMask groundLayer;
     public int maxJumps = 2;
     int jumpsRemaining;
@@ -32,38 +32,30 @@ public class PlayerController : MonoBehaviour
     }
 
     void MovePlayer()
+{
+    float moveInput = 0;
+
+    if (Input.GetKey(KeyCode.RightArrow))
+        moveInput = 1;
+
+    if (Input.GetKey(KeyCode.LeftArrow))
+        moveInput = -1;
+
+    if (Input.GetKey(KeyCode.D))
+        moveInput = 1;
+
+    if (Input.GetKey(KeyCode.A))
+        moveInput = -1;
+
+    rb.linearVelocity = new Vector2(moveInput * speed, rb.linearVelocity.y);
+
+    if (jumpsRemaining > 0 && Input.GetKeyDown(KeyCode.Space))
     {
-        if(Input.GetKey(KeyCode.RightArrow))
-        {
-            // Get the current position of the player
-            Vector2 curPos = gameObject.transform.position;
-
-            // Get new position by adding to the x coordinate
-            Vector2 newPos = new Vector2(curPos.x + Time.deltaTime * speed, curPos.y);
-
-            // Update the player's position
-            gameObject.transform.position = newPos;
-        }
-        if(Input.GetKey(KeyCode.LeftArrow))
-        {
-            // Get the current position of the player
-            Vector2 curPos = gameObject.transform.position;
-
-            // Get new position by subtracting from the x coordinate
-            Vector2 newPos = new Vector2(curPos.x - Time.deltaTime * speed, curPos.y);
-
-            // Update the player's position
-            gameObject.transform.position = newPos;
-        }
-    if(jumpsRemaining > 0)
-    {
-        if(Input.GetKeyDown(KeyCode.Space))
-        {
-            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpPower);
-            jumpsRemaining--;
-        }
+        rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpPower);
+        jumpsRemaining--;
     }
-    }
+}
+
 
     private void GroundCheck()
     {
