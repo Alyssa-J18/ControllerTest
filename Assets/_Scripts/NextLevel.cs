007a7c5c3cd1130643fa9f2b5042d1f055270b89
Alyssa-J18 <ajacoby1118@gmail.com>
2026-03-14 23:16:30 -0400

    all levels added, damage and health mechanic made, lighting, levels connected

--- Assets/_Scripts/NextLevel.cs
+++ Assets/_Scripts/NextLevel.cs
@@ -4,9 +4,36 @@ public class NextLevel : MonoBehaviour
 {
     public string nextLevelName;
 
+    private bool playerNearby = false;
+
+    void Update()
+    {
+        if (playerNearby && Input.GetKeyDown(KeyCode.E))
+        {
+            LoadNextLevel();
+        }
+    }
+
     public void LoadNextLevel()
     {
         UnityEngine.SceneManagement.SceneManager.LoadScene(nextLevelName);
         Time.timeScale = 1;
     }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            playerNearby = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            playerNearby = false;
+        }
+    }
 }
+
--- Assets/_Scripts/PlayerController1.cs
+++ Assets/_Scripts/PlayerController1.cs
@@ -1,8 +1,9 @@
 using UnityEngine;
-
+using UnityEngine.UI;
+using System.Collections;
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 0.1f;
+    public float speed = 6f;
     public Rigidbody2D rb;
     public float jumpPower = 10f;
     
@@ -11,12 +12,20 @@ public class PlayerController : MonoBehaviour
     public LayerMask groundLayer;
     public int maxJumps = 2;
     int jumpsRemaining;
+    
+    public int health = 3;
+    public Image healthImage;
+    private SpriteRenderer spriteRenderer;
+
+    public float iFrameDuration = 1f;
+    private bool isInvisible = false;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         jumpsRemaining = maxJumps;
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -29,6 +38,8 @@ public class PlayerController : MonoBehaviour
         {
             Die();
         }
+
+        healthImage.fillAmount = health / 3f;
     }
 
     void MovePlayer()
@@ -68,6 +79,47 @@ public class PlayerController : MonoBehaviour
     {
         Gizmos.color = Color.white;
         Gizmos.DrawWireCube(groundCheckPos.position, groundCheckSize);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Damage"))
+        {
+            if (isInvisible) return;
+            health -= 1;
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpPower);
+            StartCoroutine(InvincibilityFrames());
+
+            if (health <= 0)
+            {
+                Die();
+            }
+        }
+    }
+
+
+    private IEnumerator BlinkRed()
+    {
+        spriteRenderer.color = Color.red;
+        yield return new WaitForSeconds(0.1f);
+        spriteRenderer.color = Color.white;
+    }
+
+    private IEnumerator InvincibilityFrames()
+    {
+        isInvisible = true;
+        float elapsed = 0f;
+        while (elapsed < iFrameDuration)
+        {
+            spriteRenderer.color = new Color(spriteRenderer.color.r, spriteRenderer.color.g, spriteRenderer.color.b, 0.3f);
+            yield return new WaitForSeconds(0.1f);
+            spriteRenderer.color = new Color(spriteRenderer.color.r, spriteRenderer.color.g, spriteRenderer.color.b, 1);
+            yield return new WaitForSeconds(0.1f);
+
+            elapsed += 0.2f;
+        }
+        spriteRenderer.color = new Color(spriteRenderer.color.r, spriteRenderer.color.g, spriteRenderer.color.b, 1);
+        isInvisible = false;
     }
         
     private void Die()
